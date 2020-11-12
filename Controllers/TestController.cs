12f36655a1eb696b391f958fb347705cd1732d5e
Rokas <rokasbarasa@gmail.com>
2020-11-12 18:14:12 +0100

    Pray to api gods for working

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebService.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class TestController : Controller
     {
         public IActionResult Index()
