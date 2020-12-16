9903edfd2eb1475d7ae23c41ecd7fbe0e30ae404
Rokas <rokasbarasa@gmail.com>
2020-12-16 18:17:30 +0100

    QuickFix of doubles

--- Repository/Context/OverviewModel.cs
+++ Repository/Context/OverviewModel.cs
@@ -6,10 +6,10 @@
         public double tempAvg { get; set; } 
         public double tempMax { get; set; } 
         public int humiMin { get; set; } 
-        public int humiAvg { get; set; } 
+        public double humiAvg { get; set; } 
         public int humiMax { get; set; } 
         public int co2Min { get; set; } 
-        public int co2Avg { get; set; } 
+        public double co2Avg { get; set; } 
         public int co2Max { get; set; }
     }
 }
--- Repository/DAO/Overview/OverviewDAO.cs
+++ Repository/DAO/Overview/OverviewDAO.cs
@@ -19,18 +19,19 @@ namespace WebService.Repository.DAO.Fact_Measurement
         
         public List<OverviewModel> GetOverviewToday(string deviceEUI)
         {
-            return _context.OverviewModel.FromSqlRaw("SELECT" +
-                                          " MIN(fm.CarbonDioxide) AS co2Min, AVG(fm.CarbonDioxide) AS co2Avg, MAX(fm.CarbonDioxide) AS co2Max," +
-                                          " MIN(fm.HumidityPercentage) AS humiMin, AVG(fm.HumidityPercentage) AS humiAvg, MAX(fm.HumidityPercentage) AS humiMax," +
-                                          " MIN(fm.Temperature) AS tempMin, AVG(fm.Temperature) AS tempAvg, MAX(fm.Temperature) AS tempMax" +
-                                          " FROM DW.F_Measurement fm" +
-                                          " INNER JOIN DW.DateDim d ON fm.DateDimKey = d.DateDimKey" +
-                                          " INNER JOIN DW.DeviceDim dv ON fm.DeviceDimKey = dv.DeviceDimKey" +
-                                          " WHERE d.Date >= DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0)" +
-                                          " AND dv.DeviceEUI = " +
-                                          "{0}" +
-                                          " AND NOT fm.Temperature = 0" +
-                                          " AND NOT fm.HumidityPercentage = 0", deviceEUI).ToList();
+            return _context.OverviewModel.FromSqlRaw(
+                "SELECT" +
+                " MIN(fm.CarbonDioxide) AS co2Min, AVG(CAST(fm.CarbonDioxide AS FLOAT)) AS co2Avg,     MAX(fm.CarbonDioxide) AS co2Max," +
+                " MIN(fm.HumidityPercentage) AS humiMin, AVG(CAST(fm.HumidityPercentage AS FLOAT)) AS humiAvg, MAX(fm.HumidityPercentage) AS humiMax," +
+                " MIN(fm.Temperature) AS tempMin, AVG(fm.Temperature) AS tempAvg, MAX(fm.Temperature) AS tempMax" +
+                " FROM DW.F_Measurement fm" +
+                " INNER JOIN DW.DateDim d ON fm.DateDimKey = d.DateDimKey" +
+                " INNER JOIN DW.DeviceDim dv ON fm.DeviceDimKey = dv.DeviceDimKey" +
+                " WHERE d.Date >= DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0)" +
+                " AND dv.DeviceEUI = " +
+                "{0}" +
+                " AND NOT fm.Temperature = 0" +
+                " AND NOT fm.HumidityPercentage = 0", deviceEUI).ToList();
         }
 
         public List<OverviewModel> GetOverviewLastWeek(string deviceEUI)
